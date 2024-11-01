424bfbbcb0313bade1dd79df30a138c853efa2e6
Lucas <amorimrodrigueslucas@gmail.com>
2024-11-01 10:34:43 -0300

    feat: estilização da login

--- Agentto/src/pages/Login/index.jsx
+++ Agentto/src/pages/Login/index.jsx
@@ -1,58 +1,63 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './style.css';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./style.css";
 
 const Login = () => {
-  const [login, setLogin] = useState('');
-  const [senha, setSenha] = useState('');
+  const [login, setLogin] = useState("");
+  const [senha, setSenha] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [rememberMe, setRememberMe] = useState(false);
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const handleLogin = () => {
-    
     if (login === "admin" && senha === "password") {
       if (rememberMe) {
-        localStorage.setItem('login', login);
-        localStorage.setItem('senha', senha);
+        localStorage.setItem("login", login);
+        localStorage.setItem("senha", senha);
       } else {
-        localStorage.removeItem('login');
-        localStorage.removeItem('senha');
+        localStorage.removeItem("login");
+        localStorage.removeItem("senha");
       }
-      navigate('/home'); 
+      navigate("/home");
     } else {
-      alert('Login ou senha incorretos');
+      alert("Login ou senha incorretos");
     }
   };
 
   return (
     <div className="login-container">
       <h2>Login</h2>
-      <input 
-        type="text" 
-        value={login} 
-        onChange={(e) => setLogin(e.target.value)} 
-        placeholder="Login" 
+      <input
+        className="inputGeral"
+        type="text"
+        value={login}
+        onChange={(e) => setLogin(e.target.value)}
+        placeholder="Login"
       />
-      <input 
-        type={showPassword ? "text" : "password"} 
-        value={senha} 
-        onChange={(e) => setSenha(e.target.value)} 
-        placeholder="Senha" 
+      <input
+        className="inputGeral"
+        type={showPassword ? "text" : "password"}
+        value={senha}
+        onChange={(e) => setSenha(e.target.value)}
+        placeholder="Senha"
       />
       <button onClick={() => setShowPassword(!showPassword)}>
         {showPassword ? "Esconder Senha" : "Mostrar Senha"}
       </button>
       <div>
-        <input 
-          type="checkbox" 
-          checked={rememberMe} 
-          onChange={() => setRememberMe(!rememberMe)} 
+        <input
+          type="checkbox"
+          checked={rememberMe}
+          onChange={() => setRememberMe(!rememberMe)}
         />
         <label>Gravar Senha</label>
       </div>
-      <button onClick={handleLogin}>Entrar</button>
-      <button onClick={() => navigate('/register')}>Cadastrar-se</button>
+      <button onClick={handleLogin} className="btnEntrar">
+        Entrar
+      </button>
+      <button onClick={() => navigate("/register")} className="btnRegister">
+        Cadastrar-se
+      </button>
     </div>
   );
 };
